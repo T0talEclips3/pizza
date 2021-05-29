3913dbf5259ace81a247fcf67b28f267fd57e652
Alexander Vorotnikov <AlexanderVorotnikov@hotmail.com>
2021-05-29 10:21:07 +0300

    added button memoization

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -11,28 +11,30 @@ export interface IButtonProps {
   clickHandler?: React.MouseEventHandler<HTMLButtonElement>;
 }
 
-const Button = ({
-  children,
-  outline,
-  circle,
-  add,
-  back,
-  cart,
-  pay,
-  clickHandler,
-}: IButtonProps) => {
-  return (
-    <button
-      onClick={clickHandler}
-      className={`button${outline ? " button--outline" : ""}${
-        circle ? " button--circle" : ""
-      }${add ? " button--add" : ""}${back ? " go-back-btn" : ""}${
-        cart ? " button--cart" : ""
-      }${pay ? " pay-btn" : ""}`}
-    >
-      {children}
-    </button>
-  );
-};
+const Button = React.memo(
+  ({
+    children,
+    outline,
+    circle,
+    add,
+    back,
+    cart,
+    pay,
+    clickHandler,
+  }: IButtonProps) => {
+    return (
+      <button
+        onClick={clickHandler}
+        className={`button${outline ? " button--outline" : ""}${
+          circle ? " button--circle" : ""
+        }${add ? " button--add" : ""}${back ? " go-back-btn" : ""}${
+          cart ? " button--cart" : ""
+        }${pay ? " pay-btn" : ""}`}
+      >
+        {children}
+      </button>
+    );
+  }
+);
 
 export default Button;
